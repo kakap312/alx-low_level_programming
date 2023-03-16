9a56dfc324df8c8857afc06d8786acc53d78b001
stephen omari <stephenomari12@gmail.com>
2023-03-16 16:08:25 -0700

    my comment

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main entry of the program
+ *
+ * Return: always xero is success
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
