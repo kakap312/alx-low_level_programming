e68b2cb17b16ea81ec94bfd8e1ba346209d542de
stephen omari <stephenomari12@gmail.com>
2023-03-16 16:43:12 -0700

    task 7 commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main entry of the program
+ *
+ * Return: always xero is success
+ */
+
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float m;
+printf("Size of a char: %2d byte(s)\n", sizeof(a));
+printf("Size of an int: %2d byte(s)\n", sizeof(b));
+printf("Size of a long int: %2dbyte(s)\n", sizeof(c));
+printf("Size of a long long int: %2dbyte(s)\n", sizeof(d));
+printf("Size of a float: %2d byte(s)\n", sizeof(m));
+return (0);
+}
