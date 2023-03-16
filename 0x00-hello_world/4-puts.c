facab88fcfba95986eb130279fc532968bcc8186
stephen omari <stephenomari12@gmail.com>
2023-03-16 15:35:59 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - this is the main entry of the program
+ *
+ * Return: always xero is success
+ */
+
 int main(void)
 {
 /* my first program in C */
-puts("Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
