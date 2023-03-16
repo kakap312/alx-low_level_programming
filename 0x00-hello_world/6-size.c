513859f6edd5480bb7a881d354c59e143e77865a
stephen omari <stephenomari12@gmail.com>
2023-03-16 16:47:05 -0700

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ long long int d;
 float m;
 printf("Size of a char: %2d byte(s)\n", sizeof(a));
 printf("Size of an int: %2d byte(s)\n", sizeof(b));
-printf("Size of a long int: %2dbyte(s)\n", sizeof(c));
-printf("Size of a long long int: %2dbyte(s)\n", sizeof(d));
+printf("Size of a long int: %2d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %2d byte(s)\n", sizeof(d));
 printf("Size of a float: %2d byte(s)\n", sizeof(m));
 return (0);
 }
