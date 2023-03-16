2d69a3982419b28db44b535c9aa7cf342f3611a4
stephen omari <stephenomari12@gmail.com>
2023-03-16 16:51:09 -0700

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %2d byte(s)\n", sizeof(a));
 printf("Size of an int: %2d byte(s)\n", sizeof(b));
 printf("Size of a long int: %2d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %2d byte(s)\n", sizeof(d));
-printf("Size of a float: %2d byte(s)\n", sizeof(m));
+printf("Size of a float: %2d byte(s)", sizeof(m));
 return (0);
 }
