399984928e53786155b24b432f135efc51e78fbd
stephen omari <stephenomari12@gmail.com>
2023-03-17 16:29:36 -0700

    new commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+	 * This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand();
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero", n);
+	} else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	return (0);
+}
