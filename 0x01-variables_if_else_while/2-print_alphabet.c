77b74f6ff79717402b8fdbb940803633f8755b67
stephen omari <stephenomari12@gmail.com>
2023-03-18 12:27:29 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ *
+ * Return: termination point
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
