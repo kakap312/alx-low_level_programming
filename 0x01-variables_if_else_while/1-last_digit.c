44c0b3ea6b421b8e6f05129e10be61f25ae91984
stephen omari <stephenomari12@gmail.com>
2023-03-18 04:07:49 -0700

    this is a new commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this is the main entry of the program
+ *
+ * Return: always xero is success
+ */
+int main(void)
+{
+        int n;
+	int lastDigit;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	printf("Last digit of %d is ", n);
+        if (lastDigit > 5)
+        {
+                printf("%d and is greater than 5 \n", lastDigit);
+        } else if (lastDigit == 0)
+        {
+                printf("%d and is 0 \n", lastDigit);
+        } else if (lastDigit < 6 && lastDigit != 0)
+        {
+                printf("%d and is less than 6 and not 0 \n", lastDigit);
+        }
+        return (0);
+}
