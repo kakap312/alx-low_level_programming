ed33a4033c535075fdf75ae4f3df38f82b8d6e67
stephen omari <stephenomari12@gmail.com>
2023-03-18 15:09:39 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  main - entry point
+ *
+ * Return: termination of program
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (a == 113 || a == 114)
+		{
+			continue;
+		} else
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
