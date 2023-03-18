24cd8b173914be116825f7e385f127fc0ced8adf
stephen omari <stephenomari12@gmail.com>
2023-03-18 16:10:08 -0700

    new commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: exit point
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
