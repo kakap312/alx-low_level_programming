ce5f7f120f8a9d91da36ff0bc0c58d2ab2fda1fa
stephen omari <stephenomari12@gmail.com>
2023-03-18 12:56:38 -0700

    new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: termination point
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	for (b = 60; b <= 90; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
