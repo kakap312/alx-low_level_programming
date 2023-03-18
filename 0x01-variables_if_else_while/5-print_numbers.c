fd6f32f545fc117cd5cf29e005b9cc3458d59393
stephen omari <stephenomari12@gmail.com>
2023-03-18 15:26:38 -0700

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: termination of program
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
